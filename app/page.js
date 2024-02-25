d32ccde47fd65753e6a63450207bf00f7b2e2b89
weii1501 <vodacluc15@gmail.com>
2024-02-25 16:18:56 +0700

    af

--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
 import HeroSection from '@/app/components/HeroSection'
 import AboutSection from '@/app/components/AboutSection'
 import ContactSection from '@/app/components/ContactSection'
+import { NextSeo } from 'next-seo'
 
 export default function Home () {
   return (
     <main className='w-full h-auto'>
+      {/*<NextSeo*/}
+      {/*  title='Vo Dac Luc - Portfolio'*/}
+      {/*  description='Vo Dac Luc - Portfolio'*/}
+      {/*/>*/}
       <HeroSection />
       <AboutSection />
       <ContactSection />
